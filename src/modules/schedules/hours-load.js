ed98a0a5ce438652e8c7c9622e6bab4d241a7f8e
Drarkors <rafaelfzanetti@hotmail.com>
2024-06-07 20:37:35 -0300

    :sparkles: Rendering hours from selected date

--- src/modules/schedules/hours-load.js
+++ src/modules/schedules/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours";
 
+const hoursList = document.getElementById("hours");
+
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     const [scheduledHour] = hour.split(":");
@@ -13,4 +15,31 @@ export function hoursLoad({ date }) {
       available: isHourPast,
     };
   });
+
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+
+    li.textContent = hour;
+
+    if (hour === "9:00") {
+      hourHeaderAdd("Manhã");
+    } else if (hour === "13:00") {
+      hourHeaderAdd("Tarde");
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite");
+    }
+
+    hoursList.append(li);
+  });
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+  header.textContent = title;
+
+  hoursList.append(header);
 }
