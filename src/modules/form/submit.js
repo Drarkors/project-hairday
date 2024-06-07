ab6ea14530cb924587bf843a7fc66f1fbeb25f5c
Drarkors <rafaelfzanetti@hotmail.com>
2024-06-07 20:52:52 -0300

    :sparkles: Getting form data and preparing a new appointment on submit

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 const today = dayjs(new Date()).format("YYYY-MM-DD");
@@ -11,4 +12,32 @@ selectedDate.min = today;
 
 form.onsubmit = (event) => {
   event.preventDefault();
+
+  try {
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Informe o nome do cliente!");
+    }
+
+    const hourSelected = document.querySelector(".hour-selected");
+
+    if (!hourSelected) {
+      return alert("Selecione a hora do atendimenot!");
+    }
+
+    const [hour] = hourSelected.innerText.split(":");
+
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+    const id = new Date().getTime();
+
+    console.log({
+      id,
+      name,
+      when,
+    });
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possível realizar o agendamento");
+  }
 };
