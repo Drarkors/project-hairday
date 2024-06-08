59a4b42ec21018103d470f92296f5ed0a75427d6
Drarkors <rafaelfzanetti@hotmail.com>
2024-06-08 00:11:27 -0300

    :sparkles: Adding schedule cancel API call and refreshing scheduled list on cancelation

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,7 +1,10 @@
+import { schedulesDay } from "./load.js";
+import { scheduleCancel } from "../../services/schedule-cancel.js";
+
 const periods = document.querySelectorAll(".period");
 
 periods.forEach((periods) => {
-  periods.addEventListener("click", (event) => {
+  periods.addEventListener("click", async (event) => {
     if (event.target.classList.contains("cancel-icon")) {
       const item = event.target.closest("li");
       const { id } = item.dataset;
@@ -13,6 +16,9 @@ periods.forEach((periods) => {
 
         if (isConfirmed) {
           // Remove scheduled hour
+          await scheduleCancel({ id });
+
+          schedulesDay();
         }
       }
     }
