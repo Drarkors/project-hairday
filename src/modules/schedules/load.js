f817f8bdfa2ef23d7970a72c7a190c0c44f13864
Drarkors <rafaelfzanetti@hotmail.com>
2024-06-07 23:30:46 -0300

    :sparkles: Rendering day schedules

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,3 +1,4 @@
+import { scheduleShow } from "./show.js";
 import { hoursLoad } from "../form/hours-load";
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day";
 
@@ -8,6 +9,9 @@ export async function schedulesDay() {
 
   // Fetch day schedules on API
   const daySchedules = await scheduleFetchByDay({ date });
+
+  // Render day schedules
+  scheduleShow({ daySchedules });
 
   // Render available hours
   hoursLoad({ date });
