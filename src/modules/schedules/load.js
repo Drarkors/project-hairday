c9424b6209fba57350e9b536d7ba35a17af583cd
Drarkors <rafaelfzanetti@hotmail.com>
2024-06-07 22:37:38 -0300

    :sparkles: Fetching day schedules on page load

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,13 @@
 import { hoursLoad } from "../form/hours-load";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day";
 
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   const date = selectedDate.value;
+
+  // Fetch day schedules on API
+  const daySchedules = await scheduleFetchByDay({ date });
 
   // Render available hours
   hoursLoad({ date });
